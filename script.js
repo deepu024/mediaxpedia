c00d73c441f4af5763df41c8ee5721d7217199ff
nupur2024 <nupur@flyheadmedia.com>
2024-07-24 17:47:28 +0530

    updated today

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navbarToggle = document.getElementById('navbar-toggle');
-    const navbarCenter = document.getElementById('navbar-center');
-
-    navbarToggle.addEventListener('click', function() {
-        navbarCenter.style.display = navbarCenter.style.display === 'flex' ? 'none' : 'flex';
-    });
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
 });
